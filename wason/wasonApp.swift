4c08f67a0b50772554559ae1deb24a6c0790070b
WasanFahad44 <wasan107@icloud.com>
2024-09-26 17:07:06 +0300

    Initial Commit

---
+++ wason/wasonApp.swift
@@ -0,0 +1,17 @@
+//
+//  wasonApp.swift
+//  wason
+//
+//  Created by wasan on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct wasonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
